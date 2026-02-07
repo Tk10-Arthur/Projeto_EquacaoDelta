2239874df96b53da5c3f94779f81744285238efc
Arthur <arthurtorresbernardino@gmail.com>
2026-02-07 20:38:44 -0300

    Projeto

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        Sistema conexoes = new Sistema();
+        
+        do {
+           conexoes.entradaDados();
+           conexoes.calcularDelta();
+           conexoes.resultadoRaizes();
+        
+        } while (!conexoes.saidaPrograma().equals("sim"));
+        
+        System.out.print("Programa encerrado.");
+        conexoes.entrada.close();
+    }
+}
---
+++ Sistema.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Sistema {
+    Scanner entrada = new Scanner(System.in);
+    double a, b, c;
+    double delta;
+    String sair;
+    
+    void entradaDados() {
+        System.out.print("\nDigite o coeficiente A: ");
+        a = entrada.nextDouble();
+
+        System.out.print("Digite o coeficiente B: ");
+        b = entrada.nextDouble();
+
+        System.out.print("Digite o coeficiente C: ");
+        c = entrada.nextDouble();
+    }
+    
+    void calcularDelta() {
+        delta = (b * b) - (4 * a * c);
+    }
+
+    void resultadoRaizes() {
+        if (delta > 0) {
+            double x1 = ((-b + Math.sqrt(delta)) / (2 *a));
+            double x2 = ((-b - Math.sqrt(delta)) / (2 *a));
+
+            System.out.print("\nX1: " + x1 + " / " + "X2: " + x2);
+
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            System.out.print("\nX: " + x);
+
+        } else {
+            System.out.print("\nNão existe raízes reais!");
+        }
+    }
+
+    String saidaPrograma() {
+        System.out.print("\nDeseja encerrar o programa? (sim / não): ");sair = entrada.next().toLowerCase();
+        return sair;
+    }
+}
